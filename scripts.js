0d26a24c14f229e63f6252644d0be7dfae8189e9
Ana Luísa Rogério Pimenta <anapimentainf@gmail.com>
2019-12-11 20:33:33 -0300

    Função que cuida do login

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-//const fetch = require('node-fetch');
 
-//const { URLSearchParams } = require('url');
 const apiUrl = 'https://login.microsoftonline.com/be87ed09-e753-468f-8244-e2f3811ceacc/oauth2/v2.0/token';
+const client_id = '682aac27-6b4b-4f13-a4f9-21f8b0f30f08';
+const client_secret = '.C=T8S7[XpPu0qY-HTPcUgVObr4=bHUF';
 
 const headers = {
     'Content-Type': 'application/x-www-form-urlencoded', 
@@ -10,8 +10,9 @@ const headers = {
 
 const body = new URLSearchParams();
 body.append('grant_type', 'password');
-body.append('client_id', '682aac27-6b4b-4f13-a4f9-21f8b0f30f08');
-body.append('client_secret', '.C=T8S7[XpPu0qY-HTPcUgVObr4=bHUF');
+body.append('client_id', client_id);
+body.append('client_secret', client_secret);
+
 body.append('scope', 'https://graph.microsoft.com/.default');
 body.append('userName', 'guildafront@FELChagashotmail.onmicrosoft.com');
 body.append('password', '94PgyhlUV7Te');
@@ -22,4 +23,30 @@ const config = {
     method: 'POST', body, headers
 };
 
-fetch(apiUrl, config).then(res => res.json()).catch(res => console.error('Houve um erro: ', res.message)).then(objetoResposta => console.log(objetoResposta));
+login();
+
+//funcao que executa o login 
+async function login(){
+    const response = await fetch(apiUrl, config);
+    
+    const objetoResposta = await response.json()
+
+    console.log(response.status);
+    console.log(objetoResposta);
+
+}
+
+
+
+
+
+// fetch(apiUrl, config)
+// .then(res => res.json())
+// .catch(res => console.error('Houve um erro: ', res.message))
+// .then(objetoResposta => console.log(objetoResposta));
+
+
+
+//900023 email invalido 
+//50034 usuario nao existe 
+//50126 invalid username or password 
